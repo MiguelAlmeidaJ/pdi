69bfe226965a39374a1188648ea75e1b9ff119d0
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-25 14:38:57 -0300

    feat: add qualifications (apps/api/src/qualifications/dto/create-qualification.dto.ts)

---
+++ apps/api/src/qualifications/dto/create-qualification.dto.ts
@@ -0,0 +1,9 @@
+import { QualificationType } from '@prisma/client';
+import { IsEnum, IsOptional, IsString, IsUrl, MinLength } from 'class-validator';
+
+export class CreateQualificationDto {
+  @IsString() @MinLength(2) name!: string;
+  @IsEnum(QualificationType) type!: QualificationType;
+  @IsString() @IsOptional() description?: string;
+  @IsUrl() @IsOptional() referenceUrl?: string;
+}
