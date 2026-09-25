669f1b9e2033a25046bc981bdea849183e0411a4
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-25 14:39:06 -0300

    feat: add qualifications (apps/api/src/qualifications/qualifications.module.ts)

---
+++ apps/api/src/qualifications/qualifications.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { QualificationsController } from './qualifications.controller';
+import { QualificationsService } from './qualifications.service';
+
+@Module({ controllers: [QualificationsController], providers: [QualificationsService] })
+export class QualificationsModule {}
