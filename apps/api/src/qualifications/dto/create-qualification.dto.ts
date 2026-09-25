147c6c337e4f24b96420a730e20246a345f8028b
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-25 16:36:26 -0300

    feat: require team on qualification creation

--- apps/api/src/qualifications/dto/create-qualification.dto.ts
+++ apps/api/src/qualifications/dto/create-qualification.dto.ts
@@ -2,6 +2,7 @@ import { QualificationType } from '@prisma/client';
 import { IsEnum, IsOptional, IsString, IsUrl, MinLength } from 'class-validator';
 
 export class CreateQualificationDto {
+  @IsString() teamId!: string;
   @IsString() @MinLength(2) name!: string;
   @IsEnum(QualificationType) type!: QualificationType;
   @IsString() @IsOptional() description?: string;
