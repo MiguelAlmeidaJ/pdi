be64044aca17b8a507c85be74cb42373b1977bdc
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-25 11:27:49 -0300

    fix: load root env in Nest workspace

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,7 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['../../.env', '.env'],
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
