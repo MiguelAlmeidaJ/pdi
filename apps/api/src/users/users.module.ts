82553777e4efb85efcdff6c2e939b3ed373feb3e
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-25 14:47:04 -0300

    feat: register development services

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { DevelopmentService } from './development.service';
+import { UserQualificationsService } from './user-qualifications.service';
 
-@Module({ controllers: [UsersController], providers: [UsersService] })
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService, DevelopmentService, UserQualificationsService],
+})
 export class UsersModule {}
